be4d427d3427fcba29c0d0e7504aa3084abf83bf
Brett Martin <bamartindev@gmail.com>
2022-12-01 13:07:52 -0600

    TypeScript Day 1 Complete

--- TypeScript/src/day-01/index.ts
+++ TypeScript/src/day-01/index.ts
@@ -1,8 +1,44 @@
-const p1 = () => {};
+import { openFileD } from '../shared/fs';
 
-const p2 = () => {};
+interface Elf {
+  food: number[];
+  total: number;
+}
+
+const mkElf = (food: string[]): Elf => {
+  const transformed = food.map((f) => parseInt(f, 10));
+  return { food: transformed, total: transformed.reduce((acc, i) => acc + i, 0) };
+};
+
+const p1 = (elves: Elf[]) => {
+  console.log('Day 1, part 1: ', elves[0].total);
+};
+
+const p2 = (elves: Elf[]) => {
+  const total = elves[0].total + elves[1].total + elves[2].total;
+  console.log('Day 1, part 2: ', total);
+};
+
+const chunkInput = (input: string[]): string[][] => {
+  return input.reduce(
+    (acc: string[][], c) => {
+      if (c === '') {
+        acc.push([]);
+      } else {
+        acc[acc.length - 1].push(c);
+      }
+      return acc;
+    },
+    [[]],
+  );
+};
 
 export const run = () => {
-  p1();
-  p2();
+  const input = openFileD('./src/day-01/input.txt', '\n');
+  const elves = chunkInput(input)
+    .map(mkElf)
+    .sort((a, b) => b.total - a.total);
+
+  p1(elves);
+  p2(elves);
 };
